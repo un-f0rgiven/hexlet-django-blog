22ba4230b119913e4e6cacddf7f470063c3d620e
un-f0rgiven <barr85@mail.ru>
2024-10-19 23:27:46 +1000

    update file

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleFormCreateView
+from hexlet_django_blog.article.views import IndexView, ArticleFormView, ArticleFormCreateView, ArticleFormEditView
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='articles_index'),
-    path('<int:id>/', ArticleView.as_view(), name='article'),
+    path('<int:id>/', ArticleFormView.as_view(), name='article'),
     path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
+    path('<int:id>/edit/', ArticleFormEditView.as_view(), name='articles_update'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -8,21 +8,18 @@ from django.contrib import messages
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         articles = Article.objects.all()[:15]
-
         return render(request, 'articles/index.html', context={'articles': articles,})
 
 
-class ArticleView(View):
+class ArticleFormView(View):
     def get(self, request, *args, **kwargs):
         article = get_object_or_404(Article, id=kwargs['id'])
-
         return render(request, 'articles/show.html', context={'article': article,})
 
 
 class ArticleFormCreateView(View):
     def get(self, request, *args, **kwargs):
         form = ArticleForm()
-
         return render(request, 'articles/create.html', context={'form': form})
     
     def post(self, request, *args, **kwargs):
@@ -33,3 +30,19 @@ class ArticleFormCreateView(View):
             return redirect('articles_index')
         messages.error(request, 'Статья не создана. Все поля должны быть заполнены')
         return render(request, 'articles/create.html', {'form': form})
+
+class ArticleFormEditView(View):
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        return render(request, 'articles/update.html', {'form': form, 'article_id': article_id})
+    
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles_index')
+        return render(request, 'articles/update.html', {'form': form, 'article_id': article_id})
